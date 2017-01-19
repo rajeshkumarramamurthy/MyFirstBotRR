c259035d6422172179061cf8982d35d0fa03be7b
Rajesh <rajeshkumar.ramamurthy@intel.com>
2017-01-19 10:06:36 +0530

    added cred

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ server.listen(process.env.PORT || 3000, function()
 
 // Create chat bot
 var connector = new builder.ChatConnector
-({ appId: 'YourAppId', appPassword: 'YourAppPassword' }); 
+({ appId: '75a5dc7a-3c70-47da-a913-7da8fd65741d', appPassword: '6njK62ADsGAuKTHjyS6T2Ud' }); 
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
